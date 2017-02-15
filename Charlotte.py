43e1cdb47f432ea6d661729a1e7072ad12a19f0d
Brooks Becton <broabect@ut.utm.edu>
2017-02-14 18:54:43 -0600

    Can remove HTML tags now

--- Charlotte.py
+++ Charlotte.py
@@ -1,12 +1,20 @@
+from bs4 import BeautifulSoup
+
+
 class Charlotte:
 
-    def remove_html_tags(self, html):
-        parsed_html = ""
-        return parsed_html
+    target_words = []
+
+    def remove_html_tags(self, raw_html):
+        clean_text = BeautifulSoup(raw_html, "html.parser").stripped_strings
+        for clean_strs in clean_text:
+            for clean_str in clean_strs.split():
+                self.target_words.append(clean_str)
+        return self.target_words
 
     def remove_stop_words(self):
         print("remove_stop_words")
 
     def get_stem_words(self):
         print("get_stem_words")
-        
+
--- test_charlotte.py
+++ test_charlotte.py
@@ -7,9 +7,29 @@ class charlotte_test(unittest.TestCase):
 
     test_charlotte = Charlotte()
 
+    def tearDown(self):
+        self.test_charlotte.target_words = []
+
     def test_remove_html_single_element(self):
         test_string = "<h1>Hello, World</h1>"
-        excpectied_result = "Hello, World"
+        excpectied_result = ["Hello,", "World"]
         self.assertEqual(self.test_charlotte.remove_html_tags(test_string), excpectied_result)
+
+    def test_remove_html_single_element_with_attr(self):
+        test_string = "<a href='//www.reddit.com/r/ProgrammerHumor/'>Hello, World</a>"
+        excpectied_result = ["Hello,", "World"]
+        self.assertEqual(self.test_charlotte.remove_html_tags(test_string), excpectied_result)
+    def test_remove_html_two_element(self):
+        test_string = "<a href='www.reddit.com/r/ProgrammerHumor/'>Hello, World</a><p>so funny</p>"
+        excpectied_result = ["Hello,", "World", "so", "funny"]
+        self.assertEqual(self.test_charlotte.remove_html_tags(test_string), excpectied_result)
+
+    def test_remove_html_empty_element(self):
+        test_string = "<p></p>"
+        excpectied_result = []
+        self.assertEqual(self.test_charlotte.remove_html_tags(test_string), excpectied_result)
+
+
+
 
 unittest.main()
