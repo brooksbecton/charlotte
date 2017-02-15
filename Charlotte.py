d30bfd20d86c815d48642c694850cbccd16d02b2
Brooks Becton <broabect@ut.utm.edu>
2017-02-14 19:57:13 -0600

    Added function and test for reomoving stop words

--- Charlotte.py
+++ Charlotte.py
@@ -1,19 +1,33 @@
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
 
 
 class Charlotte:
 
     target_words = []
+    stop_words = []
+
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
 
     def remove_html_tags(self, raw_html):
+        #Removes all html tags and concats all strings
         clean_text = BeautifulSoup(raw_html, "html.parser").stripped_strings
+
+        #Creating list of strings
         for clean_strs in clean_text:
             for clean_str in clean_strs.split():
                 self.target_words.append(clean_str)
         return self.target_words
 
-    def remove_stop_words(self):
-        print("remove_stop_words")
+    def remove_stop_words(self, unfiltered_words):
+        filtered_words = []
+
+        for word in unfiltered_words:
+            if word not in self.stop_words:
+                filtered_words.append(word)
+
+        return filtered_words
 
     def get_stem_words(self):
         print("get_stem_words")
--- test_charlotte.py
+++ test_charlotte.py
@@ -7,6 +7,7 @@ class charlotte_test(unittest.TestCase):
 
     test_charlotte = Charlotte()
 
+    #HTML Removal Tests
     def tearDown(self):
         self.test_charlotte.target_words = []
 
@@ -29,7 +30,25 @@ class charlotte_test(unittest.TestCase):
         excpectied_result = []
         self.assertEqual(self.test_charlotte.remove_html_tags(test_string), excpectied_result)
 
+    def test_remove_stop_word_the(self):
+        test_list = ["the", "dog"]
+        excpectied_result = ["dog"]
+        self.assertEqual(self.test_charlotte.remove_stop_words(test_list), excpectied_result)
 
+    def test_remove_stop_words(self):
+        test_string = ['me', 'couldn', 'up', 'down', 'because', 'his', 'to', 'both', 'those', 'having', 'into', 'her', 'been', 'here', 'again', 'few', 'he', 'most', 'over', 'haven', 'when', 'than', 'myself', 'yourselves', 'no', 'all', 'the', 'now', 'am', 'under', 'shouldn', 'it', 'isn', 'each', 'can', 'but', 'did', 't', 'these', 'where', 'doesn', 'by', 'is', 'as', 'my', 'and', 'theirs', 'yours', 'so', 'there', 'which', 'if', 'only', 'why', 'had', 'their', 'who', 'does', 'after', 'any', 'we', 'yourself', 'same', 'has', 'ours', 'through', 'i', 'ma', 'until', 'too', 'do', 'its', 'd', 'were', 'a', 'of', 'itself', 'during', 'wouldn', 'them', 'further', 'hadn', 'aren', 'very', 'not', 'while', 'an', 'for', 'your', 'or', 'from', 'own', 'weren', 'themselves', 'what', 'y', 'didn', 'against', 'above', 'more', 'needn', 've', 'o', 'himself', 'ourselves', 'below', 'on', 'ain', 'will', 'about', 'once', 'such', 'doing', 'are', 'our', 'herself', 'm', 'out', 'hasn', 'hers', 'be', 'being', 'have', 'they', 'mightn', 'some', 'at', 'him', 'just', 'should', 'between', 'this', 'was', 'll', 'shan', 'whom', 'wasn', 'you', 'off', 'in', 's', 'then', 'don', 'nor', 're', 'how', 'she', 'other', 'that', 'won', 'with', 'mustn', 'before']
+        excpectied_result = []
+        self.assertEqual(self.test_charlotte.remove_stop_words(test_string), excpectied_result)
+
+    def test_remove_stop_words_none(self):
+        test_string = ["cat", "dog", "parrot", "dinosaur"]
+        excpectied_result = ["cat", "dog", "parrot", "dinosaur"]
+        self.assertEqual(self.test_charlotte.remove_stop_words(test_string), excpectied_result)
+
+    def test_remove_stop_words_no_words(self):
+        test_string = []
+        excpectied_result = []
+        self.assertEqual(self.test_charlotte.remove_stop_words(test_string), excpectied_result)
 
 
 unittest.main()
