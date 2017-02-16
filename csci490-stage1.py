ac1914311cc52f7f339d5d12f99b380c7921c382
Brooks Becton <broabect@ut.utm.edu>
2017-02-15 23:08:33 -0600

    Wrote driver for stage 1 :car:

---
+++ csci490-stage1.py
@@ -0,0 +1,31 @@
+from Charlotte import Charlotte
+
+import os
+import json
+
+msC = Charlotte()
+
+DIRECTORIES = ['assets/projectdata/test/course/', 'assets/projectdata/test/faculty/', 'assets/projectdata/test/student/']
+# DIRECTORIES = ['assets/projectdata/test/student/']
+
+i = 0
+
+for directory in DIRECTORIES:
+    FILENAMES = msC.get_all_filenames_from_dir(directory, '.html')
+
+    msC.process_html_files(FILENAMES)
+
+    DATA = msC.word_data
+
+    JSON_DATA = json.dumps(DATA, indent=4, sort_keys=True)
+    if not os.path.exists('results'):
+        os.makedirs('results')
+    filename = 'results/results-' + str(i) + ' .json'
+
+    with open(filename, 'w') as f:
+        f.write(JSON_DATA)
+
+    msC.word_data = {}
+
+    i += 1
+        
